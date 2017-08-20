72423043424658f93635d37cbc78e32e57b060d1
Hoang Nguyen <Hoangnguyenatn@gmail.com>
2017-08-20 12:43:35 +0700

    Create cookie.php

---
+++ very_ez/cookie.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic CTF</title>
+</head>
+<body style="background-image:url(https://s-media-cache-ak0.pinimg.com/originals/13/8b/d7/138bd70605e7a638d391eb51f7eae8c0.jpg); background-size:1400px;background-repeat: no-repeat ;background-attachment: fixed;">
+
+
+<h3 style="color: blue;text-align: center;" > người đừng lặng im đến thế , vì lặng im sẽ giết chết con tim</h3>
+<!-- very easy :you can't see it , find it-->
+
+
+<?php
+setcookie("flag","very_easy_for_it",time()+600,"/",0);
+
+
+?>
+
+
+
+
+</body>
+</html>
